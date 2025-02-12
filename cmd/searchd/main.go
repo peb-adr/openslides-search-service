fb40e97c655e2ae50b3d276a42c719043ab2040f
Bastian Rihm <brihm@intevation.de>
2025-02-12 16:56:11 +0100

    Use `openslides-go` (#199)

--- cmd/searchd/main.go
+++ cmd/searchd/main.go
@@ -15,9 +15,9 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	"github.com/OpenSlides/openslides-autoupdate-service/pkg/auth"
-	"github.com/OpenSlides/openslides-autoupdate-service/pkg/environment"
-	"github.com/OpenSlides/openslides-autoupdate-service/pkg/redis"
+	"github.com/OpenSlides/openslides-go/auth"
+	"github.com/OpenSlides/openslides-go/environment"
+	"github.com/OpenSlides/openslides-go/redis"
 	"github.com/OpenSlides/openslides-search-service/pkg/config"
 	"github.com/OpenSlides/openslides-search-service/pkg/meta"
 	"github.com/OpenSlides/openslides-search-service/pkg/oserror"
